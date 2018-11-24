55228b56263bbe7532d9b1929ce7a064224084ae
Sean Fox <geekypghrunner@gmail.com>
2018-11-23 20:00:06 -0500

    fix: iOS hover on projects

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "98d814713fe555782ddd";
+/******/ 	var hotCurrentHash = "6fbf28827409a17f89e5";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- _next/static/webpack/styles.98d814713fe555782ddd.hot-update.js
+++ _next/static/webpack/styles.98d814713fe555782ddd.hot-update.js
@@ -1,4 +1,4 @@
-(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["styles"],{
+webpackHotUpdate("styles",{
 
 /***/ "./css/styles.css":
 /*!************************!*\
@@ -29,7 +29,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1543020769771");
+            injectCss(link, link.href.split("?")[0] + "?unix=1543020756207");
           });
       }
     }
@@ -37,5 +37,5 @@
 
 /***/ })
 
-}]);
-//# sourceMappingURL=styles.js.map
+})
+//# sourceMappingURL=styles.98d814713fe555782ddd.hot-update.js.map
