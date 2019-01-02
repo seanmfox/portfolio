91f8f97de2766549655db24937bb28372278d964
Sean Fox <geekypghrunner@gmail.com>
2019-01-02 18:11:09 -0500

    deploy: Update available static pages

---
+++ _next/static/webpack/static/development/pages/blog.js.199722aec2300f1a6c84.hot-update.js
@@ -0,0 +1,54 @@
+webpackHotUpdate("static/development/pages/blog.js",{
+
+/***/ "./components/Header.js":
+/*!******************************!*\
+  !*** ./components/Header.js ***!
+  \******************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! next/link */ "./node_modules/next/link.js");
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_link__WEBPACK_IMPORTED_MODULE_1__);
+
+
+
+var Header = function Header() {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("header", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("nav", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "left-header"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+    src: "../static/img/sf_avatar.png",
+    alt: "My Avatar",
+    className: "header-image"
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "left-header-text"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", null, "Sean Fox"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h4", null, "Web Developer"))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "right-header"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "menu-toggle"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("input", {
+    type: "checkbox"
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "hamburger"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("i", null), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("i", null), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("i", null)), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("ul", {
+    className: "nav-links"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(next_link__WEBPACK_IMPORTED_MODULE_1___default.a, {
+    href: "/"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
+    className: "right-header-text"
+  }, "Home"))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(next_link__WEBPACK_IMPORTED_MODULE_1___default.a, {
+    href: "/about"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
+    className: "right-header-text"
+  }, "About"))))))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("hr", null));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Header);
+
+/***/ })
+
+})
+//# sourceMappingURL=blog.js.199722aec2300f1a6c84.hot-update.js.map
